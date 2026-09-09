c72dc511386ac1f5f0cabdc05b27d0e26c58f66e
vova <vk4935391@gmail.com>
2026-09-09 19:11:11 +0300

    feat: add redis cache serialization with jakson

--- src/main/java/volodea/gymbookingsystem/config/CacheConfig.java
+++ src/main/java/volodea/gymbookingsystem/config/CacheConfig.java
@@ -1,9 +1,23 @@
 package volodea.gymbookingsystem.config;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        RedisSerializer<Object> serializer = RedisSerializer.json();
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+                );
+    }
 }
